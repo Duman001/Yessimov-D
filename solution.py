16173578803142e7045890017d3a351eff801b1d
Duman001 <duman.1337l@gmail.com>
2020-04-09 01:08:02 +0500

    7777777

---
+++ solution.py
@@ -0,0 +1,11 @@
+a = int(input())
+p1 = "   _~_    "
+p2 = "  (o o)   "
+p3 = " /  V  \  "
+p4 = "/(  _  )\ "
+p5 = "  ^^ ^^   "
+print(p1 * a, sep="")
+print(p2 * a)
+print(p3 * a)
+print(p4 * a)
+print(p5 * a)
